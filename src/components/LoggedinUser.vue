cee11279d6351fac1c6d7f0c3815db42233a71d1
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-08 07:23:03 +0800

    Add login user section

---
+++ src/components/LoggedinUser.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <q-list>
+    <q-item class="q-px-none">
+      <q-item-section avatar>
+        <q-avatar
+          color="accent"
+          text-color="white"
+        >M</q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label class="text-weight-medium text-subtitle1">
+          ajikun
+        </q-item-label>
+        <q-item-label class="text-grey-8">ajikun@gmail.com</q-item-label>
+      </q-item-section>
+      <q-item-section side>
+        <q-btn
+          flat
+          color="primary"
+          label="logout"
+        />
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
