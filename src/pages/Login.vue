8995af9989ba1f3ce9f3d848fcf21c4630ca4d23
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-08 07:52:22 +0800

    Add login logic

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,26 +4,60 @@
       <q-card-section class="q-pb-none">
         <div class="text-h6">Welcome</div>
       </q-card-section>
-      <q-form>
+      <q-form @submit.prevent="handleSubmit">
         <q-card-section class="q-gutter-sm">
           <q-input
             outlined
             label="Username"
+            v-model="username"
           />
           <q-input
             outlined
+            type="password"
             label="Password"
+            v-model="password"
           />
         </q-card-section>
         <q-separator />
         <q-card-actions class="bg-grey-2">
           <q-btn
             flat
+            type="submit"
             color="primary"
             label="login"
+            :loading="isLoading"
           />
         </q-card-actions>
       </q-form>
     </q-card>
   </div>
 </template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      isLoading: false
+    }
+  },
+  methods: {
+    async handleSubmit () {
+      this.isLoading = true
+      try {
+        const token = await this.$axios.post('http://localhost:8000/v1/auth/login', {
+          username: this.username,
+          password: this.password
+        })
+        this.$q.localStorage.set('token', token)
+        this.$router.push({ name: 'home' })
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.isLoading = false
+      }
+    }
+  }
+}
+</script>
