49832a93a6c3f1f1bd1d8d297c21576f1e0d6055
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-09-08 07:23:08 +0800

    Add comment input

---
+++ src/components/CommentInput.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="q-my-sm">
+    <q-input
+      rounded
+      outlined
+      autogrow
+      placeholder="Write your comment..."
+    >
+      <template v-slot:prepend>
+        <q-btn
+          flat
+          round
+          icon="emoji_emotions"
+        />
+      </template>
+      <template v-slot:append>
+        <q-btn
+          flat
+          round
+          icon="photo"
+        />
+        <q-btn
+          flat
+          round
+          color="primary"
+          icon="send"
+        />
+      </template>
+    </q-input>
+  </div>
+</template>
